f3a4833a165507bc08a2c5447dccc9b1b94513e4
katirantala <katirantala2@gmail.com>
2024-10-09 15:44:57 +0300

    Header and image added.

--- src/components/ComponentUI.jsx
+++ src/components/ComponentUI.jsx
@@ -1,21 +1,26 @@
 import { useState } from 'react';
 import './ComponentUI.css';
+import image from '../assets/images/shop.png';
 import OrderForm from './OrderForm.jsx';
 import OrderInfo from './OrderInfo.jsx';
 
-function ComponentUI() {
+const Header = ({ image, title} ) => {
+  return (
+    <div className='Header'>
+      <img src={image} alt={title} className="circle-image"/>
+      <h1>{title}</h1>
+    </div>
+  );
+};
 
+function ComponentUI() {
 return (
-    <>
       <div>
-        <h1>Otsikko</h1>
+        <Header image={image} title="Welcome to product page!" />
         <OrderForm />
         <OrderInfo />
       </div>
-    </>
-  )
+    )
 }
-
-
 
 export default ComponentUI;
