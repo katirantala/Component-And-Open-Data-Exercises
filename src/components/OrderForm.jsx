5c83514ff2f7d5b5033158908e8210f6b6c87166
katirantala <katirantala2@gmail.com>
2024-10-09 13:36:53 +0300

    Counter added

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,25 +1,33 @@
 import { useState } from 'react';
 import './OrderForm.css';
 
+//Products
 const products = [
     { id: 1, name: 'Laptop', price: 1000 },
     { id: 2, name: 'Television', price: 1200 },
     { id: 3, name: 'Phone', price: 600 },
 ];
 
+//Function for select product
+
 function OrderForm() {
     const [selectedProduct, setSelectedProduct] = useState(''); 
+    const [count, setCount] = useState(0)
 
     const handleChange = (e) => {
         const productId = parseInt(e.target.value);
         const product = products.find(p => p.id === productId);
         setSelectedProduct(product);
     };
+    //Counter funktions
+        const addCount = () => { setCount(count + 1); };
+        
+        const decreaseCount = () => { setCount(count > 0 ? count - 1 : 0);};
 
     return (
         <div>
             <h2>Select Product</h2>
-            Product: 
+            Product: {' '} 
             <select onChange={handleChange} value={selectedProduct ? selectedProduct.id : ''}>
                 <option value="" disabled>Select...</option>
                 {products.map(product => (
@@ -27,7 +35,14 @@ function OrderForm() {
                         {product.name} ({product.price}€)
                     </option>
                 ))}
-            </select>
+            </select><br></br>
+            <br></br>
+            Quantity: {' '}
+            <button onClick={decreaseCount}>-</button>
+            {' '}
+            <label>{count}</label>
+            {' '}
+            <button onClick={addCount}>+</button>
         </div>
     );
 }
