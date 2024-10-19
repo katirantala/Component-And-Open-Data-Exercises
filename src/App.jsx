34bfa0055872424032bbe7837869e0d59275cbb3
katirantala <katirantala2@gmail.com>
2024-10-19 19:36:46 +0300

    Routing branch and code added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ComponentUI from './components/ComponentUI';
 import DogImage from './components/Opendata';
+import './App.css';
 
 function App() {
-  
-
   return (
-    <>
-      <div>
-        <ComponentUI />
-        <DogImage />
-      </div>
-    </>
-  )
+    <Router>
+      <nav>
+            <Link to="/">Product page</Link>{' '}
+            <Link to="/dog-image">Dog image</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ComponentUI />} />
+        <Route path="/dog-image" element={<DogImage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
